c3417756dcbc2637a202856fad7daf7562cac06e
Amar <amarpratap2208@gmail.com>
2022-02-26 22:50:05 +0530

    Provide right path to install_dependencies again

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
-python scripts/install_dependencies.py
+python src/scripts/install_dependencies.py
 
 torchserve --start --model-store ./serve/baseline/model_store --models resnet-18=resnet-18.mar --ncs
