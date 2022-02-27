514f347b909156bccbc5aaf3c9e5f38d71537292
Amar <amarpratap2208@gmail.com>
2022-02-27 13:52:59 +0530

    add apt-get update to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 echo '**********debug**********'
 which apt-get
 
+apt-get update
+
 python src/scripts/install_dependencies.py
 
 torchserve --start --model-store ./serve/baseline/model_store --models resnet-18=resnet-18.mar --ncs
